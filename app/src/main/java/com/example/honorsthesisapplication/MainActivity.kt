f055b0638dbd01d4e55b31098d35897926a23204
alyss <alyssaduranovic@gmail.com>
2025-11-10 01:09:08 -0700

    Basic Vibration Alert Button Button on phone that sends vibration alert to watch.

--- app/src/main/java/com/example/honorsthesisapplication/MainActivity.kt
+++ app/src/main/java/com/example/honorsthesisapplication/MainActivity.kt
@@ -1,41 +1,59 @@
 package com.example.honorsthesisapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.Wearable
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Greeting(
-                    name = "Android",
-                    modifier = Modifier.padding(innerPadding)
-                )
-            }
+            AlertButtonView(
+                onSendAlert = { sendMessageToWatch("/vibrate") }
+            )
         }
     }
+
+    private fun sendMessageToWatch(path: String) {
+        Thread {
+            val nodes = Tasks.await(
+                Wearable.getNodeClient(this).connectedNodes
+            )
+            Log.d("Node: ", "here")
+            for(node in nodes) {
+                Log.d("Node: ", node.id)
+                Wearable.getMessageClient(this)
+                    .sendMessage(node.id, path, null)
+            }
+        }.start()
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("Android")
+fun AlertButtonView(onSendAlert: () -> Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button( onClick = { onSendAlert() }) {
+            Text("Send Alert")
+        }
+    }
 }
--- wear/src/main/java/com/example/honorsthesisapplication/data/source/PhoneDataSource.kt
+++ wear/src/main/java/com/example/honorsthesisapplication/data/source/PhoneDataSource.kt
@@ -1,2 +1,32 @@
 package com.example.honorsthesisapplication.data.source
 
+import android.content.Context
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.util.Log
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.WearableListenerService
+
+class PhoneDataSource : WearableListenerService() {
+
+    override fun onMessageReceived(messageEvent: MessageEvent) {
+        Log.d("Watch", "Message received: ${messageEvent.path}")
+        if (messageEvent.path == "/vibrate") {
+            vibrateWatch()
+        }
+    }
+
+    private fun vibrateWatch() {
+        val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        val vibrator = vibratorManager.defaultVibrator
+
+        //type of vibration effect
+        val effect = VibrationEffect.createOneShot(
+            500,
+            VibrationEffect.DEFAULT_AMPLITUDE
+        )
+        vibrator.vibrate(effect)
+    }
+
+}
